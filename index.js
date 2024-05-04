de4e7e3a21bb6965d1aeb18aca3af0a2d6ed0f67
LntAxe <ertusari0@gmail.com>
2024-05-04 19:16:49 +0300

    refactor: Remove comments

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 "use strict";
 
-/**
- * Decodes the email address.
- * @param {string} encodedString The encoded email address.
- * @returns {string} The decoded email address.
- */
+
+
 function decode(encodedString) {
 	let email = "";
 	const key = extractKey(encodedString);
@@ -17,11 +14,6 @@ function decode(encodedString) {
 	return email;
 }
 
-/**
- * Encodes the email address.
- * @param {string} email The email address.
- * @returns {string} The encoded email address.
- */
 function encode(email) {
 	const [key, keyHex] = generateKey();
 	const encodedString =
@@ -33,32 +25,15 @@ function encode(email) {
 	return encodedString;
 }
 
-/**
- * Extracts the key from the encoded string.
- * @param {string} encodedString The encoded string.
- * @returns {number} The key.
- * @private
- */
 function extractKey(encodedString) {
 	return parseInt(encodedString.substring(0, 2), 16);
 }
 
-/**
- * Generates a key for encoding.
- * @returns {[number, string]} The key and the key in hexadecimal format.
- * @private
- */
 function generateKey() {
 	const key = Math.floor(Math.random() * 256) + 1;
 	return [key, hexEncode(key)];
 }
 
-/**
- * Encodes a number to hexadecimal format.
- * @param {number} str The number to encode.
- * @returns {string} The hexadecimal representation of the number.
- * @private
- */
 function hexEncode(str) {
 	return str.toString(16).padStart(2, "0");
 }
